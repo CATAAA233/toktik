7b267052fa5f407ddd9f2b0f275bd66ff8a86a21
Carlos Catalan <carlosdanielcatalan1o@gmail.com>
2024-06-07 20:56:41 -0700

    Feat(Initial App, Entities, models and providers): Creating a intial structure

---
+++ lib/presentation/providers/discover_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/videos_post.dart';
+import 'package:toktik/infrastructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_posts.dart';
+
+class DiscoverProvider extends ChangeNotifier {
+  bool initialLoading = true;
+  List<VideoPost> videos = [];
+
+  Future<void> loadNextPage() async {
+    //Todo: add Repository, Data source
+
+    await Future.delayed(const Duration(seconds: 3));
+
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPost())
+        .toList();
+
+    initialLoading = false;
+    notifyListeners();
+  }
+}
