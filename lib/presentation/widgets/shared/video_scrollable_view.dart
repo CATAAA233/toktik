77a4880a40bd6e7421ef60acf8946046e806502e
Carlos Catalan <carlosdanielcatalan1o@gmail.com>
2024-06-07 23:01:39 -0700

    Feat(Full Screen player): Adding a full Screen payer widget

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/videos_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -11,11 +12,23 @@ class VideoScrollableView extends StatelessWidget {
     return PageView.builder(
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
+      itemCount: videos.length,
       itemBuilder: (context, index) {
         final VideoPost videoPost = videos[index];
 
         return Stack(
-          children: [Positioned(
+          children: [
+
+            // FullScreenPlayer();
+            SizedBox.expand(
+              child: FullscreenPlayer(
+                caption: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+
+              )
+            ),
+            
+            Positioned(
             bottom: 35,
             right: 30,
             child: VideoButtons(video: videoPost))],
